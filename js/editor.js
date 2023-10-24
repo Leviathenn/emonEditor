4a7888599f8c8878c1eca184352609bce6abb62d
Leviathan <86807700+Leviathenn@users.noreply.github.com>
2023-10-24 13:13:52 +0000

    aS

--- js/editor.js
+++ js/editor.js
@@ -157,57 +157,8 @@ $(document).ready(function () {
     console.log(parelem.style);
     parelem.style.color = "green";
 })
-    let timer, currSeconds = 0; 
-          
- 
-    var idleInterval = setInterval("timerIncrement()", 1000); 
-
-    // Zero the idle timer on mouse movement.
-    $(this).mousemove(function (e) {
-        // hide warning + countdown
-        countdown = 5;
-        // $('#fade').remove();  // remove the lightbox fade layer
-       // $('#idlewarn').fadeOut('fast'); // hide our warning message
-        idleTime = 0;
-    });
-    // Also Zero the timer on keypress
-    $(this).keypress(function (e) {
-        // hide warning + countdown
-        countdown = 5;
-        // $('#fade').remove();  // remove the lightbox fade layer
-       // $('#idlewarn').fadeOut('fast'); // hide the warning message
-        idleTime = 0;
-    });
-    function timerIncrement() {
-        idleTime = idleTime + 1; 
     
-        // if user has been idle for the idlemin time do this - show warning message.
-        if (idleTime >= idlemin && idleTime < idlemax) { 
     
-          countdown = (idlemax - idleTime) ; //use a countdown to show user how long they have 
-         // $('#countdown').html(countdown);
-    
-          // Show the warning along with a countdown timer
-          //if ($('#idlewarn').css('display') != 'block') {
-    
-            // Fade in the Popup and add close button
-   //         $('#idlewarn').fadeIn() 
-    
-            //Fade in Background
-            //$('body').append('<div id="fade"></div>'); //Add the fade layer to bottom of the body tag.
-       //     //$('#fade').fadeIn(); // Fade in the fade layer 
-         //   return false;
-     //     }
-                
-        } else if (idleTime >= idlemax) {
-          // User has been idle too long - they've exceeded idlemax time.
-          // so take another action
-          // in this example we simply send them back to the homepage with a logout flag
-         // window.location.href= '/?logout';
-            
-        }
-    
-    }
 
 
 });
---
+++ js/memoryPath.js
@@ -0,0 +1,8 @@
+/**
+ * @author Leviathenn
+ */
+
+!(function (w,d){
+    var canvas = document.querySelector('canvas'); 
+    
+})(window,document);
